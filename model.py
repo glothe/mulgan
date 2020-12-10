69c04cee548cfd43e7fdfbfb1f0c2714407abfc2
Timothée Darcet <timothee.darcet+git@m4x.org>
2020-12-10 18:56:47 +0100

    todo check clamp

--- model.py
+++ model.py
@@ -14,7 +14,7 @@ def normalize_image(image):
     return (image * 2 - 1).clamp(-1, 1)
 
 def denormalize_image(image):
-    return (image * .5 + .5).clamp(-1, 1)
+    return (image * .5 + .5).clamp(-1, 1)  # TODO: clamp this to (0, 1) ? 
 
 def generate_sizes(max_size=250, min_size=25, scale_factor=0.75):
     max_size = 250
