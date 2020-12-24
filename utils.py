ee67951c464d27fc467de04b250e8f6ac982a118
Timothée Darcet <timothee.darcet+git@m4x.org>
2020-12-24 14:26:56 +0100

    Handle display of multiple images

--- utils.py
+++ utils.py
@@ -21,9 +21,13 @@ def generate_sizes(max_size=250, min_size=25, scale_factor=0.75):
     print(sizes)
     return sizes[::-1]
 
-def display_image(image):
-    plt.imshow(denormalize_image(image.cpu()).permute(1, 2, 0))
+def display_image(image, ax=plt):
+    ax.axis("off")
+    ax.imshow(denormalize_image(image.cpu()).permute(1, 2, 0))
 
 def display_images(imbatch):
+    n_im = imbatch.shape[0]
+    n_rows = n_im // 4
+    axes, fig = plt.subplots(n_rows, 4)
     for i in range(imbatch.shape[0]):
-        display_image(imbatch[i])
+        display_image(imbatch[i], axes[i // 4][i % 4])
