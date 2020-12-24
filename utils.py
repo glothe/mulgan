95aacd8047ff56f4a696f2ad838621852b2bbeb0
Timothée Darcet <timothee.darcet+git@m4x.org>
2020-12-24 14:31:01 +0100

    Typo

--- utils.py
+++ utils.py
@@ -28,6 +28,6 @@ def display_image(image, ax=plt):
 def display_images(imbatch):
     n_im = imbatch.shape[0]
     n_rows = n_im // 4
-    axes, fig = plt.subplots(n_rows, 4)
+    fig, axes = plt.subplots(n_rows, 4)
     for i in range(imbatch.shape[0]):
         display_image(imbatch[i], axes[i // 4][i % 4])
