f9aea8b14a9156ea1d19aee56d9a5247401335fa
Timothée Darcet <timothee.darcet+git@m4x.org>
2020-12-25 13:00:12 +0100

    Write to the right filename

--- utils.py
+++ utils.py
@@ -44,14 +44,14 @@ def save_as_video(imbatch, fps=24, outfile="movie_{}.mp4"):
     os.makedirs("./videos", exist_ok=True)
     rmtree('./videos/tmp', ignore_errors=True)
     os.makedirs("./videos/tmp", exist_ok=True)
-    out_fn = next(filterfalse(os.path.exists, map(outfile.format, count())))
-    print(f"Writing video to {out_fn}")
+    out_fn_formatted = next(filterfalse(os.path.exists, map(outfile.format, count())))
+    print(f"Writing video to {out_fn_formatted}")
     for i in range(imbatch.shape[0]):
         save_image(denormalize_image(imbatch[i]), f"./videos/tmp/tmpim_{i}.png")
     try:
         ffmpeg\
             .input(f'./videos/tmp/tmpim_*.png', pattern_type="glob", framerate=fps)\
-            .output(f"videos/{outfile}")\
+            .output(f"videos/{out_fn_formatted}")\
             .run(capture_stdout=True, capture_stderr=True)
     except ffmpeg.Error as e:
         print('stdout:', e.stdout.decode('utf8'))
